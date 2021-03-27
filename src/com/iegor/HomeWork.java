01697f33137db15408a7a818440cdca82bf57c18
IegorNemolovskyi <iegor.nemolovskyi.t@gmail.com>
2021-03-27 16:23:32 +0200

    Arrays v1

--- src/com/iegor/HomeWork.java
+++ src/com/iegor/HomeWork.java
@@ -1,13 +1,29 @@
 package com.iegor;
 
+import java.util.Scanner;
+
 public class HomeWork {
     public static void main(String[] args) {
-        System.out.println(hw1task1(4));
-        System.out.println(hw1task2(153));
-        System.out.println(hw2task1(10101));
-        System.out.println(hw2task2(11));
-        System.out.println(hw2task3(4));
+        // System.out.println(hw1task1(4));
+        // System.out.println(hw1task2(153));
+        // System.out.println(hw2task1(10101));
+        // System.out.println(hw2task2(11));
+        // System.out.println(hw2task3(4));
+        
+        int a = 1;
+        int [] nums = new  int[10];
+        System.out.println(nums);
+        fillNums(nums);
+        // int sumResult = sumArray(nums);
+        // System.out.println(sumResult);
+        printNums(nums); //hw3task1(nums);
+        //System.out.println(hw3task1);
+        nums = new int [] {4, 100, 3, 4};
+        printNums(nums); 
+        fillFromConsole(nums);
+        printNums(nums);
     }
+    
     public static int hw1task1(int n){
         // For a given integer n calculate the value which is equal to a:
         // • squared number, if its value is strictly positive; 
@@ -24,6 +40,7 @@ public class HomeWork {
         else
             return 0;
     }
+
     public static int hw1task2(int n){
         // Find the maximum integer, that can be obtained by permutation of numbers of an arbitrary three-digit positive integer n (100<=n<=999).
         // Example:
@@ -44,6 +61,7 @@ public class HomeWork {
         }
         return result;
     }
+
     public static int hw2task1(int value){
         // For a positive integer n calculate the result value, which is equal to the sum of the odd numbers of n.
         // Example,
@@ -62,6 +80,7 @@ public class HomeWork {
             throw new IllegalArgumentException();
         }
     }
+
     public static int hw2task2(int value) {
         // For a positive integer n calculate the result value, which is equal to the amount of the “1” in the binary
         // representation of n.
@@ -80,6 +99,7 @@ public class HomeWork {
             throw new IllegalArgumentException();
         }
     }
+
     public static int hw2task3(int value) {
         // For a positive integer n, calculate the result value, which is equal to the sum of the first n Fibonacci numbers.
         // Note. Fibonacci numbers are a series of numbers in which each next number is equal to the sum of the two
@@ -104,4 +124,49 @@ public class HomeWork {
             throw new IllegalArgumentException();
         }
     }
+
+    public static void printNums(int [] nums) {  //public static void hw3task1 (int [] nums)
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println(" nums[" + i + "] = " + nums[i]);
+        }
+
+    }
+
+    public static int sumArray(int [] nums) {
+        int sum = 0;
+        for (int elem: nums) {
+            sum+=elem;
+        }
+        return sum;
+    }
+
+    public static void fillNums(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            nums [i] = i*i;
+        }
+    }
+
+    public static void fillFromConsole(int [] nums) {
+        Scanner sc = new Scanner(System.in);
+                for (int i = 0; i < nums.length; i++) {
+                    while(true){
+                    try{
+                    System.out.println(" Enter nums[" + i + "]" );
+                    nums [i] = sc.nextInt();
+                    break;
+                    }catch (Exception e) {
+                        System.err.println("Error: input not INT");
+                        sc.nextLine();
+
+                    }
+                    }
+        }
+        sc.close();
+    }
+
+    public static void isInRange(int n) {
+        
+        
+    }
+
 }
